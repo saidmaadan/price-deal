10980978a634174a426225cc5e365f3846b3de57
Said Maadan <me@saidmaadan.com>
2019-01-24 22:24:11 -0600

    Products creation in prisma and schema.graphql

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,12 @@
-const mutations = {};
+const Mutations = {
+    async createProduct(parent, args, ctx, info){
+        const product = await ctx.db.mutation.createProduct({
+            data:{
+                ...args
+            }
+        }, info);
+        return product
+    }
+};
 
-module.exports = mutations;
+module.exports = Mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,11 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+    products: forwardTo('db')
+    // async products(parent, args, ctx, info){
+    //     const products = await ctx.db.query.products();
+    //     return products;
+    // }
+};
 
 module.exports = Query;
