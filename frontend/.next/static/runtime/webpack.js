a0ed6ddd8590d9312db71f5f84d55d6d108d017c
Said Maadan <me@saidmaadan.com>
2019-02-06 09:12:34 -0600

    Implement password reset flow w with Mailtrap email provider

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a03d661be2fe9e6bac64";
+/******/ 	var hotCurrentHash = "9eea069652afc5afe8c6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -1,5 +1,6 @@
 import Signup from '../components/Signup';
 import Signin from '../components/Signin';
+import RequestReset from '../components/RequestReset';
 import styled from 'styled-components';
 
 const Columns = styled.div`
@@ -12,7 +13,8 @@ const SignupPage = () => (
     <Columns>
         <Signup />
         <Signin />
-  </Columns>
+        <RequestReset />
+    </Columns>
 );
 
 export default SignupPage;
